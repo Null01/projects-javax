f1e9bafddb728889a33eee22cce079eac38a6db0
andresduran0502@gmail.com <andresduran0502@gmail.com@47bf382c-62ce-ff38-cb5d-c3363cc1cd9c>
2014-04-27 18:40:41 +0000

    Commit. Project - add class prime in package mathematics

---
+++ JSE_Algorithms/src/mathematics/Prime.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathematics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author duran
+ * @version 1.0
+ */
+public class Prime {
+
+    private final int MAX_COUNT_PRIME;
+    public final boolean prime[];
+
+    public Prime(int MAX_COUNT_PRIME) {
+        this.MAX_COUNT_PRIME = MAX_COUNT_PRIME;
+        prime = new boolean[MAX_COUNT_PRIME + 1];
+        criba_eratostenes();
+    }
+
+    private void criba_eratostenes() {
+        for (int i = 2; i * i <= MAX_COUNT_PRIME; i++) {
+            if (!prime[i]) {
+                for (int k = 2; (i * k) <= MAX_COUNT_PRIME; k++) {
+                    prime[i * k] = true;
+                }
+            }
+        }
+    }
+
+    public List<Integer> getNumbersPrime(boolean[] bs) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i < bs.length; i++) {
+            if (!bs[i]) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+}
