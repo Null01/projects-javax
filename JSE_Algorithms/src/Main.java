0462744b9e1a49cc20fb3d2adcd5ac8564e249e1
andresduran0502@gmail.com <andresduran0502@gmail.com@47bf382c-62ce-ff38-cb5d-c3363cc1cd9c>
2014-05-18 19:52:45 +0000

    Project JSE_Algorithm - Main practice

---
+++ JSE_Algorithms/src/Main.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author duran
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String args[]) throws FileNotFoundException, IOException {
+        //BufferedReader in = new BufferedReader(new FileReader("input"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+    }
+}
